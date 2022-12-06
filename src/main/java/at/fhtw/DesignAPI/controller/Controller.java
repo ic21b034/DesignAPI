765f5ce26b42c692cc156f711dbca2645ce68d15
wahid <ic21b034@technikum-wien.at>
2022-12-06 18:53:22 +0100

    create controller package and classe

---
+++ src/main/java/at/fhtw/DesignAPI/controller/Controller.java
@@ -0,0 +1,15 @@
+package at.fhtw.DesignAPI.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class Controller {
+
+    @GetMapping("/")
+    String hello() {
+        return"Hello";
+    }
+
+}
